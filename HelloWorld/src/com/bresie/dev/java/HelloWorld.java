22ec8856ed0db8f897b4fb9b3738147b72b43859
Eric Bresie <ebresie@gmail.com>
2016-01-09 09:57:51 -0600

    This is an initial attempt at using github to capture a basic Java Hello World Eclipse Project into the repository.

---
+++ HelloWorld/src/com/bresie/dev/java/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.bresie.dev.java;
+/**
+ * 
+ */
+
+/**
+ * @author eric
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
