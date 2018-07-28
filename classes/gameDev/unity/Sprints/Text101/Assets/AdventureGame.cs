ba45a4952c8f07dababd6a16949dfd267c795b78
Eric Bresie <ebresie@gmail.com>
2018-07-28 17:12:50 -0500

    More state updates and initial basic key handling

--- classes/gameDev/unity/Sprints/Text101/Assets/AdventureGame.cs
+++ classes/gameDev/unity/Sprints/Text101/Assets/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -18,6 +19,28 @@ public class AdventureGame : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        ManageState();
 		
 	}
+
+    private void ManageState()
+    {
+        // var allows variables to be defined with a type based on the initaliziing value
+        var nextState = state.GetNextStates();
+        var numberOfStates = nextState.Length;
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            state = nextState[0];
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextState[1];
+        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextState[2];
+        } else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            state = nextState[3];
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
 }
