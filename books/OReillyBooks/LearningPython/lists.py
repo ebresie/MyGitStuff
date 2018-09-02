1bc4d0c5a872afcda1743cdfe28b70bfc9d6bccf
ebresie <ebresie@gmail.com>
2018-09-02 08:48:18 -0500

    additional prints to list example

--- books/OReillyBooks/LearningPython/lists.py
+++ books/OReillyBooks/LearningPython/lists.py
@@ -2,11 +2,11 @@ L=[123, 'spam',1.23];
 print(L);
 print('len=',len(L))
 # Indexing by position 123 
-L[0]
-# Slicing a list returns a new list [123, 'spam'           
-L[:-1]                           
+print(L[0])
+# Slicing a list returns a new list [123, 'spam']
+print(L[:-1])
 
 # Concat/repeat make new lists too [123, 'spam', 1.23, 4, 5, 6]
-L + [4, 5, 6]
+print(L + [4, 5, 6])
 
-L * 2
+print(L * 2)
